176333b9578fc830a8474b03643a71d40ab5139a
Mr. Guo <474224024@qq.com>
2016-05-24 10:44:31 +0800

    获取推荐位管理内容

--- Application/Admin/Controller/PositionController.class.php
+++ Application/Admin/Controller/PositionController.class.php
@@ -16,6 +16,12 @@ class PositionController extends Controller{
 
     public function index()
     {
+        $result = D('Position')->getPosition();
+        if ($result === false){
+            return show(0,'获取数据失败' );
+        }
+
+        $this->assign('res',$result);
         $this->display();
     }
 }
--- Application/Common/Model/PositionModel.class.php
+++ Application/Common/Model/PositionModel.class.php
@@ -14,5 +14,9 @@ class PositionModel extends Model{
     {
         $this->_db = M('position');
     }
+    public function getPosition(){
+        $data['status'] = array('neq',-1);
+        return $this->_db->where($data)->select();
+    }
 
 }
