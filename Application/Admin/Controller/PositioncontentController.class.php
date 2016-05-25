aba357ef9bc32aea6e6e2c2a60cd598793937ae3
Mr. Guo <474224024@qq.com>
2016-05-25 12:47:08 +0800

    7-4完成推荐列表功能

--- Application/Admin/Controller/PositioncontentController.class.php
+++ Application/Admin/Controller/PositioncontentController.class.php
@@ -16,6 +16,35 @@ class PositioncontentController extends Controller{
 
     public function index()
     {
+        $positions = D('Position')->getPosition();
+        //获取推荐位里面的内容
+        $data['status'] = array('neq',-1);
+        if ($_GET['title']){
+            $data['title'] = trim($_GET['title']);
+            $this->assign('title',$data['title']);
+        }
+        $data['position_id'] = $_GET['position_id'] ? intval($_GET['position_id']) : $positions[0]['id'];
+        $contents = D('PositionContent')->select($data);
+        $this->assign('positions',$positions);
+        $this->assign('positionId',$data['position_id']);
+        $this->assign('contents',$contents);
         $this->display();
+    }
+    public function setStatus(){
+        if ($_POST){
+            $id = $_POST['id'];
+            $status = $_POST['status'];
+            try {
+                $res = D('PositionContent')->updateStatusById($id, $status);
+                if ($res){
+                    return show(1,"操作成功" );
+                }else{
+                    return show(0,"操作失败" );
+                }
+            }catch (Exception $e){
+                return show(0,$e->getMessage() );
+            }
+            return show(0,"没有提交的数据" );
+        }
     }
 }
--- Application/Common/Model/PositionContentModel.class.php
+++ Application/Common/Model/PositionContentModel.class.php
@@ -17,5 +17,25 @@ class PositionContentModel extends Model{
     public function insert($data){
         return $this->_db->add($data);
     }
-
+    public function select($data=array(),$limit=0){
+        if ($data['title']){
+            $data['title'] = array('like','%'.$data['title'].'%');
+        }
+        $this->_db->where($data)->order('listorder desc,id desc');
+        if ($limit){
+            $this->_db->limit($limit);
+        }
+        $list = $this->_db->select();
+        return $list;
+    }
+    public function updateStatusById($id,$status){
+        if ( !$id || !is_numeric($id)){
+            throw_exception("ID不合法");
+        }
+        if (!is_numeric($status)){
+            throw_exception("状态不合法");
+        }
+        $data['status'] = $status;
+        return $this->_db->where('id='.$id)->save($data);
+    }
 }
