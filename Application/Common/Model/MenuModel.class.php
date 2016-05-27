9ae539e990d1fae7e3e7d9509f85252c51062305
Mr. Guo <474224024@qq.com>
2016-05-27 10:58:09 +0800

    9-2站点信息和导航栏的数据填充
    Menu模型获取数据修行为获取状态为1的正常数据

--- Application/Common/Model/MenuModel.class.php
+++ Application/Common/Model/MenuModel.class.php
@@ -73,4 +73,12 @@ class MenuModel extends Model{
         );
         return $this->_db->where($data)->order('listorder desc,menu_id desc')->select();
     }
+    public  function getBarMenus(){
+        $data=array(
+            'status'=>1,
+            'type'=>0,
+        );
+        $res = $this->_db->where($data)->order('listorder desc,menu_id desc')->select();
+        return $res;
+    }
 }
